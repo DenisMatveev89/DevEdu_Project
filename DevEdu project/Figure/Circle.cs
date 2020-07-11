e3fc818b14551e9db74b71e1f9c3fcfd0e636f0e
Anastasia <anastasila.mik@yandex.ru>
2020-07-11 17:59:58 +0300

    Add Strategy to Circle and Ellipse
    Circle and Ellipse GetPoints are created

--- DevEdu project/Figure/Circle.cs
+++ DevEdu project/Figure/Circle.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DevEdu_project.GetPoints;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -9,39 +10,12 @@ namespace DevEdu_project
 {
     public class Circle : AFigure
     {
-        public double R;   
-
-        public new List<Point> GetPoints()
+        public Circle()
         {
-            List<Point> circle = new List<Point>();
-            int X0 = _startPoint.X;
-            int Y0 = _startPoint.Y;
-            int x = 0;
-            int y = (int)R;
-            int delta = 1 - 2 * (int)R;
-            int error;
-            while (y >= 0)
-            {
-                circle.Add(new Point(X0 + x, Y0 + y));
-                circle.Add(new Point(X0 + x, Y0 - y));
-                circle.Add(new Point(X0 - x, Y0 + y));
-                circle.Add(new Point(X0 - x, Y0 - y));
-
-                error = 2 * (delta + y) - 1;
-                if ((delta < 0) && (error <= 0))
-                {
-                    delta += 2 * ++x + 1;
-                    continue;
-                }
-                if ((delta > 0) && (error > 0))
-                {
-                    delta -= 2 * --y + 1;
-                    continue;
-                }
-                delta += 2 * (++x - --y);
-            }
-            return circle;
+            getPoints = new CirclePoints();
         }
+
+        public double R;       
 
     }
 }
---
+++ DevEdu project/GetPoints/EllipsePoints.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevEdu_project.GetPoints
+{
+    public class EllipsePoints : IGetPoints
+    {
+        public List<Point> GetPoints(Point start, Point end)
+        {
+            List<Point> ellipse = new List<Point>();
+
+            double RX = Math.Sqrt(Math.Pow((end.X - start.X), 2));
+            double RY = Math.Sqrt(Math.Pow((end.Y - start.Y), 2));
+
+            int centerX = start.X;
+            int centerY = start.Y;
+            int radiusX = (int)RX;
+            int radiusY = (int)RY;
+
+            int posX = radiusX;
+            int posY = 0;
+
+            int deltaX = 2 * radiusY * radiusY * posX;
+            int deltaY = 2 * radiusX * radiusX * posY;
+            int err = (radiusX * radiusX) - (radiusY * radiusY * radiusX) + (radiusY * radiusY) / 4;
+
+            while (deltaY < deltaX)
+            {
+                ellipse.Add(new Point(centerX + posX, centerY + posY));
+                ellipse.Add(new Point(centerX + posX, centerY - posY));
+                ellipse.Add(new Point(centerX - posX, centerY + posY));
+                ellipse.Add(new Point(centerX - posX, centerY - posY));
+                posY++;
+
+                if (err < 0)
+                {
+                    deltaY += 2 * radiusX * radiusX;
+                    err += deltaY + radiusX * radiusX;
+                }
+                else
+                {
+                    posX--;
+                    deltaY += 2 * radiusX * radiusX;
+                    deltaX -= 2 * radiusY * radiusY;
+                    err += deltaY - deltaX + radiusX * radiusX;
+                }
+            }
+
+            err = radiusX * radiusX * (posY * posY + posY)
+                + radiusY * radiusY * (posX - 1) * (posX - 1)
+                - radiusY * radiusY * radiusX * radiusX;
+
+            while (posX >= 0)
+            {
+                ellipse.Add(new Point(centerX + posX, centerY + posY));
+                ellipse.Add(new Point(centerX + posX, centerY - posY));
+                ellipse.Add(new Point(centerX - posX, centerY + posY));
+                ellipse.Add(new Point(centerX - posX, centerY - posY));
+                posX--;
+
+                if (err > 0)
+                {
+                    deltaX -= 2 * radiusY * radiusY;
+                    err += radiusY * radiusY - deltaX;
+                }
+                else
+                {
+                    posY++;
+                    deltaY += 2 * radiusX * radiusX;
+                    deltaX -= 2 * radiusY * radiusY;
+                    err += deltaY - deltaX + radiusY * radiusY;
+                }
+            }
+
+            return ellipse;
+        }
+    }
+}
