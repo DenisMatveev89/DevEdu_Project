83f27046a407da8d19842745d7c1c38668ddb357
AndreyKarlukov <karl911291@yandex.ru>
2020-07-15 14:30:03 +0300

    Mouse on triangle fix

--- DevEdu project/Figure/TriangleEquilateral.cs
+++ DevEdu project/Figure/TriangleEquilateral.cs
@@ -53,6 +53,10 @@ namespace DevEdu_project.Figure
             {
                 check = true;
             }
+            else if (vectorEquation1 < 0 && vectorEquation2 < 0 && vectorEquation3 < 0)
+            {
+                check = true;
+            }
             else if (vectorEquation1 ==0 || vectorEquation2 == 0 || vectorEquation3 == 0)
             {
                 check = true;
